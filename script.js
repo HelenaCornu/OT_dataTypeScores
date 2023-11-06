638f2d0be9c0ff61d4113e219d734b9b401127d2
HelenaCornu <hcornu@ebi.ac.uk>
2023-11-06 17:47:32 +0000

    :rocket:

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-function handleTableToggle() {
-  let tableBTN = document.querySelector("#table-btn");
-  let tableSection = document.querySelector("#graph-drawer");
-  tableBTN.onclick = function () {
-    let ifClosed = tableBTN.classList.contains("closed");
-    tableSection.classList.toggle("hide");
-    if (ifClosed) {
-      tableBTN.classList.remove("closed");
-      tableBTN.classList.add("open");
-    } else {
-      tableBTN.classList.remove("open");
-      tableBTN.classList.add("closed");
-    }
-  };
-}
-
-// axios.post('https://api.platform.opentargets.org/api/v4/graphql', {
-//   query: `{
-//     disease(efoId: "EFO_0001071") {
-//     associatedTargets{
-//       rows{
-//         target {
-//           approvedSymbol
-//           id
-//           approvedName
-//         }
-//         score
-//         datasourceScores {
-//           id
-//           score
-//         }
-//       }
-//     }
-//   }
-// }`
-// }).then((result) => {
-//   console.log(
-//     result.data.disease.associatedTargets.rows[0].target.approvedName);
-// });
-
 function getData() {
   // Getting the data from the OT Platform API as a post request, .then ensures that you only proceed once you have the data
   axios
@@ -64,123 +24,174 @@ function getData() {
     // Getting only the data we need
     .then((result) => {
       console.log(result.data.data.disease.associatedTargets.rows);
-      let rawElements = result.data.data.disease.associatedTargets.rows;
+      rawElements = result.data.data.disease.associatedTargets.rows;
 
       // Cutting down the number of responses. Can modify -- set to five for testing
-      rawElements = rawElements.splice(0, 5);
+      rawElements = rawElements.splice(0, 10);
 
       // Adding the data into the rows
       let tableContent = "";
       let tableContainer = document.querySelector("#table-rows");
 
+      let graphData = []; //declaring as an object
+
       for (let i = 0; i < rawElements.length; i++) {
         let currentElement = rawElements[i];
+
+        //adding data to the table -- to test I have removed class=hide from the graph drawer
         let tableTemplate = `<tr>
-        <td><button id="table-btn" class = "closed"></button></td>
+        <td><button id="table-btn-${currentElement.target.id}" class = "closed"></button></td>
         <td>${currentElement.target.approvedSymbol}</td>
         <td>${currentElement.target.id}</td>
         <td>${currentElement.target.approvedName}</td>
         <td>${currentElement.score}</td>
     </tr>
-    <tr class = "hide" id = "graph-drawer" >
+    <tr class = "hide" id = "graph-drawer-${currentElement.target.id}" >
         <td colspan = "5">
             <div class = "chartcontainer">
-            <canvas id="bar-chart"></canvas>
-            <canvas id="radar-chart"></canvas>
+            <canvas id="bar-chart-${currentElement.target.id}"></canvas>
+            <canvas id="radar-chart-${currentElement.target.id}"></canvas>
         </div>
         </td>
     </tr>`;
+
         tableContent = tableContent + tableTemplate;
-      }
+
+        //creating the object storing the chart data
+
+        //Nested for loop iterates through the datasource scores for the current element, adding each datasource name to an array and each datasource score to an array
+        // Within graphData, create a nested object for which the ID is the target ID
+
+        let targetScores = {
+          newRow: {
+            targetID: `${currentElement.target.id}`,
+            datasourceList: [],
+            datasourceScoreList: [],
+          },
+        };
+
+        for (let j = 0; j < currentElement.datasourceScores.length; j++) {
+          targetScores["newRow"]["datasourceList"].push(
+            currentElement.datasourceScores[j].id
+          );
+          targetScores["newRow"]["datasourceScoreList"].push(
+            currentElement.datasourceScores[j].score
+          );
+        } //end of nested for loop
+
+        //add to graphData
+        graphData.push(targetScores);
+      } //end of first for loop
+
+      //Adding the new HMTL back into the table
       tableContainer.innerHTML = tableContent;
-    });
-}
+      console.log(graphData);
 
-function initCharts() {
-  const data = {
-    labels: [
-      "Literature",
-      "RNA Expression",
-      "Genetic Association",
-      "Somatic Mutation",
-      "Known Drug",
-      "Animal Model",
-      "Affected Pathway",
-    ],
-    datasets: [
-      {
-        label: "Data Type Scores",
-        data: [0.696, 0.922, 0.122, 0.933, 0.983, 0.197, 0.605],
-      },
-    ],
-  };
+      // Charts and button toggle
+      for (let k = 0; k < graphData.length; k++) {
+        (async function () {
+          const data = {
+            labels: graphData[k].newRow.datasourceList,
+            datasets: [
+              {
+                label: "Data Type Scores",
+                data: graphData[k].newRow.datasourceScoreList,
+              },
+            ],
+          };
 
-  (async function () {
-    new Chart(document.getElementById("bar-chart"), {
-      type: "bar",
-      options: {
-        animation: false,
-        plugins: {
-          legend: {
-            display: false,
-          },
-          title: {
-            display: true,
-            text: "Data Type scores (bar)",
-            padding: {
-              top: 10,
-              bottom: 30,
-            },
-            font: {
-              size: 18,
-              family: "Roboto",
-            },
-          },
-        },
-      },
-      data: data,
-      backgroundColor: ["rgb(52, 137, 202)"],
-    });
-  })();
+          new Chart(
+            document.getElementById(
+              `bar-chart-${graphData[k]["newRow"]["targetID"]}`
+            ),
+            {
+              type: "bar",
+              data: data,
+              options: {
+                animation: false,
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                  title: {
+                    display: true,
+                    text: "Data Type scores (bar)",
+                    padding: {
+                      top: 10,
+                      bottom: 30,
+                    },
+                    font: {
+                      size: 18,
+                      family: "Roboto",
+                    },
+                  },
+                },
+              },
+              backgroundColor: ["rgb(52, 137, 202)"],
+            }
+          ); // end of bar chart
 
-  (async function () {
-    new Chart(document.getElementById("radar-chart"), {
-      type: "radar",
-      data: data,
-      borderColor: ["rgb(52, 137, 202)"],
-      options: {
-        animation: false,
-        plugins: {
-          legend: {
-            display: false,
-          },
-          title: {
-            display: true,
-            text: "Data Type scores (radar)",
-            padding: {
-              top: 10,
-              bottom: 30,
-            },
-            font: {
-              size: 18,
-              family: "Roboto",
-            },
-          },
-        },
-        elements: {
-          line: {
-            borderWidth: 3,
-          },
-        },
-      },
-    });
-  })();
-}
+          new Chart(
+            document.getElementById(
+              `radar-chart-${graphData[k]["newRow"]["targetID"]}`
+            ),
+            {
+              type: "radar",
+              data: data,
+              borderColor: ["rgb(52, 137, 202)"],
+              options: {
+                animation: false,
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                  title: {
+                    display: true,
+                    text: "Data Type scores (radar)",
+                    padding: {
+                      top: 10,
+                      bottom: 30,
+                    },
+                    font: {
+                      size: 18,
+                      family: "Roboto",
+                    },
+                  },
+                },
+                elements: {
+                  line: {
+                    borderWidth: 3,
+                  },
+                },
+              },
+            }
+          ); // end of radar chart
+        })(); // end of async function
+
+        let tableBTN = document.querySelector(
+          `#table-btn-${graphData[k]["newRow"]["targetID"]}`
+        );
+        let tableSection = document.querySelector(
+          `#graph-drawer-${graphData[k]["newRow"]["targetID"]}`
+        );
+
+        tableBTN.onclick = function () {
+          let ifClosed = tableBTN.classList.contains("closed");
+          tableSection.classList.toggle("hide");
+          if (ifClosed) {
+            tableBTN.classList.remove("closed");
+            tableBTN.classList.add("open");
+          } else {
+            tableBTN.classList.remove("open");
+            tableBTN.classList.add("closed");
+          }
+        };
+      } // end of chart for loop
+    }); // end of .then
+} // end of getData function
 
 function startApp() {
-  // handleTableToggle();
   getData();
-  // initCharts();
 }
 
 startApp();
